9a1380338c0c23164b2e394a302a5da94b8dc9b9
lahiru <lahirupathiranalr@gmail.com>
2017-06-17 08:49:30 +0530

    template

---
+++ template.php
@@ -0,0 +1,31 @@
+<?php
+require_once 'core/init.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<?php include_once 'auth.php'?>
+<?php include_once 'includes/header.php'?>
+
+<body>
+
+<?php //include_once 'includes/navigation.php'?>
+<div class="nav">
+    <ul>
+        <li class="home"><a href="#">Home</a></li>
+        <li class="tutorials"><a class="active" href="#">Tutorials</a></li>
+        <li class="about"><a href="#">About</a></li>
+        <li class="news"><a href="#">Newsletter</a></li>
+        <li class="contact"><a href="#">Contact</a></li>
+    </ul>
+</div>
+
+
+<div class="content">
+
+
+</div>
+
+<?php include_once 'includes/footer.php'?>
+</body>
+
+</html>
