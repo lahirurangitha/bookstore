b84d2c0101e04f4c6789118f7592e61fd982eac9
lahiru <lahirupathiranalr@gmail.com>
2017-07-08 08:47:16 +0530

    booking upload validation added

--- upload_books_process.php
+++ upload_books_process.php
@@ -8,34 +8,39 @@ if(is_dir($upload_dir)==false){
     mkdir("$upload_dir", 0700);		// Create directory if it does not exist
 }
 if(isset($_FILES['books'])){
-    $errors= array();
-    foreach($_FILES['books']['tmp_name'] as $key => $tmp_name ){
-        $file_name = $_FILES['books']['name'][$key];
-        $file_size =$_FILES['books']['size'][$key];
-        $file_tmp =$_FILES['books']['tmp_name'][$key];
-        $file_type=$_FILES['books']['type'][$key];
+    if(!$_FILES["books"]["error"] == 4) {
+        $errors= array();
+        foreach($_FILES['books']['tmp_name'] as $key => $tmp_name ){
+            $file_name = $_FILES['books']['name'][$key];
+            $file_size =$_FILES['books']['size'][$key];
+            $file_tmp =$_FILES['books']['tmp_name'][$key];
+            $file_type=$_FILES['books']['type'][$key];
 
-        $sql="INSERT into book (name,created_by,location) VALUES (?,?,?)";
-        $upload_db = DB::getInstance();
+            $sql="INSERT into book (name,created_by,location) VALUES (?,?,?)";
+            $upload_db = DB::getInstance();
 
-        if(empty($errors)==true){
-            if(!file_exists("$upload_dir/".$file_name)){
-                move_uploaded_file($file_tmp,$upload_dir."/".$file_name);
-                $location = $upload_dir."/".$file_name;
-            }else{									//rename the file if another one exist
-                rename($file_tmp,$upload_dir."/".$file_name.time()) ;
-                $location = $upload_dir."/".$file_name.time();
+            if(empty($errors)==true){
+                if(!file_exists("$upload_dir/".$file_name)){
+                    move_uploaded_file($file_tmp,$upload_dir."/".$file_name);
+                    $location = $upload_dir."/".$file_name;
+                }else{									//rename the file if another one exist
+                    rename($file_tmp,$upload_dir."/".$file_name.time()) ;
+                    $location = $upload_dir."/".$file_name.time();
+                }
+                $upload_db->query($sql,array($file_name,$user->id,$location));
+            }else{
+                print_r($errors);
             }
-            $upload_db->query($sql,array($file_name,$user->id,$location));
-        }else{
-            print_r($errors);
         }
-    }
-    if(empty($error)){
-        Session::put('messages','Successfully uploaded.');
-        Session::put('m_type', 'success');
+        if(empty($error)){
+            Session::put('messages','Successfully uploaded.');
+            Session::put('m_type', 'success');
+        }else{
+            Session::put('messages','Failed to upload.');
+            Session::put('m_type', 'error');
+        }
     }else{
-        Session::put('messages','Failed to upload.');
+        Session::put('messages','Failed to upload. No book selected.');
         Session::put('m_type', 'error');
     }
 
