975eb31209f065c7eda24cdb8bcbeabd57abb55a
lahiru <lahirupathiranalr@gmail.com>
2017-07-09 12:48:37 +0530

    booking upload validation added

--- index.php
+++ index.php
@@ -11,6 +11,24 @@ require_once 'core/init.php';
 <?php include_once 'includes/navigation.php'?>
 
 <div class="content">
+    <?php
+    if(!$user->isLoggedIn()){
+        ?>
+        <div class="col-5 col-4-offset">
+            <strong style="font-size: 75px" class="col-1-offset">Welcome</strong><br>
+            <img src="styles/img/logo.png"><br>
+
+            <h3 class="col-1-offset">Please Log In OR Register to Continue</h3>
+
+        </div>
+    <?php
+
+    }else{
+        Redirect::to('user_dashboard.php');
+    }
+    ?>
+
+
 
 </div>
 
